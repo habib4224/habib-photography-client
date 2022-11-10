8106f3370ee8cfe564d6d31bfd4f6c041ad8e0da
habib4224 <4224habib1@gmail.com>
2022-11-10 19:45:05 +0600

    about

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -2,13 +2,15 @@ import React from 'react';
 import aboutimg from '../../../Images/about/about.jpg'
 const About = () => {
     return (
-        <div className="card lg:card-side bg-base-200 shadow-xl my-6">
-            <figure><img src={aboutimg} alt="Album" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">Our Payment Gateway</h2>
-                <p>Here are a lot of payment system in my website. You can parchase any sservice from anywhere in the world.</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Pay</button>
+        <div className=" shadow-xl my-6 flex">
+            <div className='p-6'><img src={aboutimg} alt="Album" /></div>
+            <div className="justify-center mx-6 my-20">
+                <h2 className=" text-center">Newsletter </h2>
+                <p>Submit Your Email Address</p>
+
+                <div className=" flex gap-3">
+                    <input name='name' type="email" placeholder="Your email address" className="input input-ghost w-2/3  input-bordered" required />
+                    <button className="btn btn-primary">Submit</button>
                 </div>
             </div>
         </div>
